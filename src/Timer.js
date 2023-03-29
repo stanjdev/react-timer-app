f9109176b906295ad4616f18131353345fced208
Stanley Jeong <stanley.jeong@shopify.com>
2023-03-29 11:14:53 -0700

    added Timer class

---
+++ src/Timer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function createTimer(name) {
+  return {
+    name,
+    time: 0,
+    isRunning: false
+  }
+}
+
+const myTimer = createTimer('Workout');
+
+
