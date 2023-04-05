b04ad2f9c7b97a84a84147c8a5871d24bbb07991
Stanley Jeong <stanley.jeong@shopify.com>
2023-04-05 10:18:08 -0700

    fixed minute and hour display issue

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,8 +4,8 @@ const pad = (number) => {
 
 export const formatTime = (time) => {
   const ms = Math.round(time / 100) % 10;
-  const secs = Math.floor(time / 1000)  % 60;
-  const mins = Math.floor(secs / 60) % 60;
-  const hours = Math.floor(mins / 60) % 60;
+  const secs = Math.floor(time / 1000) % 60;
+  const mins = Math.floor(time / 1000 / 60) % 60;
+  const hours = Math.floor(time / 1000 / 60 / 60) % 60;
   return `${pad(hours)}:${pad(mins)}:${pad(secs)}.${ms}`;
 }
